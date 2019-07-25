e9b860497e840eb3305ad12fbe3b26233dccf87f
guolejian <guolejian@126.com>
2019-07-25 16:22:43 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+import MpvueRouterPatch from 'mpvue-router-patch'
+
+Vue.use(MpvueRouterPatch);
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+  config: {
+    // pages: [
+    //   '^pages/count/main'
+    // ],
+    // 注意，页面级可配置属性相当于只是`src/main.js`中配置里的`window`部分
+    "navigationBarTitleText": "文章列表页面",
+    "backgroundTextStyle": "light",
+    "navigationBarBackgroundColor": "#fff",
+    "navigationBarTextStyle": "black"
+  }
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container" @click="clickHandle">
+    <div class="message">{{msg}}</div>
+    <div @click="skipPage">
+      点我跳转页面
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello'
+      }
+    },
+
+    methods: {
+      clickHandle () {
+        this.msg = 'Clicked!!!!!!'
+      },
+      skipPage() {
+        this.$router.push({path: '/pages/count/index'});
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .message {
+    color: red;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
