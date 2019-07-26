856ea824a6e592c4b96bf133704fbe494feece21
guolejian <guolejian@126.com>
2019-07-26 17:27:40 +0800

    修改路由路径

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
-import store from '../store/index'
-import http from '../api/http.js'
-
+import store from '../store/index';
+import http from '../api/http.js';
+import moment from 'moment';
 // 全局store
 global.$store = Vue.prototype.$store = store;
 // 全局http请求
 global.$http = Vue.prototype.$http = http;
+// 时间工具
+global.moment = Vue.prototype.moment = moment;
