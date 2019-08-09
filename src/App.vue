fcc514c46587365a7ce735f73d84e03241d18a1b
guolejian <guolejian@126.com>
2019-08-09 17:58:04 +0800

    1.引入iconfont 2.修改我的界面

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,4 @@
 </script>
 
 <style lang="scss">
-  @import '../static/css/main.css';
 </style>
--- src/pages/my.vue
+++ src/pages/my.vue
@@ -3,18 +3,37 @@
     <div class="my-header">
       <div class="my-header-setting">
         <van-icon size="20px" name="setting-o"/>
-        <van-icon customStyle="margin-left: 15px;" size="20px" name="comment-o" @click="goMessage" />
+        <van-icon customStyle="margin-left: 15px;" size="20px" name="comment-o" @click="goMessage"/>
       </div>
       <div class="my-header-userinfo">
-        <van-row gutter="20">
+        <van-row gutter="10" class="my-header-adjust">
           <van-col span="4">
             <i-avatar :src="profileInfo.avatarUrl" size="large"></i-avatar>
           </van-col>
-          <van-col span="20" v-if="profileInfo.nickName != null">{{ profileInfo.nickName }}</van-col>
+          <van-col span="6" v-if="profileInfo.nickName != null">{{ profileInfo.nickName }}</van-col>
           <a v-if="profileInfo.nickName == null" class="text-fs-14" @click="goRegister">登陆/注册</a>
         </van-row>
       </div>
-      <div></div>
+      <div class="my-header-menu text-center">
+        <van-row>
+          <van-col span="6">
+            <div>0</div>
+            <div>优惠券</div>
+          </van-col>
+          <van-col span="6">
+            <div>0</div>
+            <div>领云钻</div>
+          </van-col>
+          <van-col span="6">
+            <div>0</div>
+            <div>我的关注</div>
+          </van-col>
+          <van-col span="6">
+            <div>0</div>
+            <div>足迹</div>
+          </van-col>
+        </van-row>
+      </div>
     </div>
     <div class="my-body">
 
@@ -76,11 +95,21 @@
     padding: 5px 10px;
     .my-header-setting {
       text-align: right;
+      padding-right: 25px;
     }
 
     .my-header-userinfo {
       height: 46px;
       line-height: 46px;
+      padding-left: 20px;
+
+      .my-header-adjust {
+        /*padding: 0 10px;*/
+      }
+    }
+
+    .my-header-menu {
+      margin-top: 10px;
     }
   }
 </style>
